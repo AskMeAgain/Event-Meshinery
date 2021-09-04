136b277277b5ef093ee88f7a6dbd3f297ca91b15
AskMeAgain <test@test.com>
2021-09-04 18:46:34 +0200

    Drawer hook test

--- modules/draw/src/test/java/ask/me/again/meshinery/draw/MeshineryDrawerTest.java
+++ modules/draw/src/test/java/ask/me/again/meshinery/draw/MeshineryDrawerTest.java
@@ -3,26 +3,55 @@ package ask.me.again.meshinery.draw;
 
 import ask.me.again.meshinery.core.common.MeshineryTask;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import java.io.IOException;
 import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
 
 class MeshineryDrawerTest {
 
   @Test
-  void testDrawerHooks() {
+  void testDrawerHooks() throws IOException {
 
+    //Arrange --------------------------------------------------------------------------------
+    var applyEdge = Mockito.mock(ApplyEdge.class);
+    var applyNode = Mockito.mock(ApplyNode.class);
+    var applyGraph = Mockito.mock(ApplyGraph.class);
+
+    var drawer = MeshineryDrawer.builder()
+      .tasks(getTasks())
+      .graphAssignment(applyGraph)
+      .nodeAssignment(applyNode)
+      .edgeAssignment(applyEdge)
+      .build();
+
+    //Act ------------------------------------------------------------------------------------
+    drawer.draw();
+
+    //Assert ---------------------------------------------------------------------------------
+    Mockito.verify(applyEdge, Mockito.times(2)).onEachEdge(any(), any());
+    Mockito.verify(applyNode, Mockito.times(3)).onEachNode(any(), any());
+    Mockito.verify(applyGraph, Mockito.times(1)).onGraph(any());
 
   }
 
   private List<MeshineryTask<?, ?>> getTasks() {
-
     return List.of(MeshineryTask.builder()
+        .read("A", null)
+        .taskName("A")
+        .write("B")
         .build(),
       MeshineryTask.builder()
+        .read("B", null)
+        .taskName("B")
+        .write("C")
+        .build(),
+      MeshineryTask.builder()
+        .read("C", null)
+        .taskName("C")
         .build()
     );
-
-
   }
-
 }
