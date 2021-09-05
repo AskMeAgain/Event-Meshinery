9192d487c68ee2fd603ee6b19fa052dd71e16fad
AskMeAgain <test@test.com>
2021-09-05 15:26:26 +0200

    MysqlProperties system

--- modules/mysql-connector-config/src/main/java/ask/me/again/meshinery/connectors/mysql/MysqlConfiguration.java
+++ modules/mysql-connector-config/src/main/java/ask/me/again/meshinery/connectors/mysql/MysqlConfiguration.java
@@ -1,15 +1,23 @@
 package ask.me.again.meshinery.connectors.mysql;
 
 import org.jdbi.v3.core.Jdbi;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties
 public class MysqlConfiguration {
 
   @Bean
-  public Jdbi jdbi() {
-    return Jdbi.create("jdbc:mysql://localhost:3306/db", "user", "password");
+  public Jdbi jdbi(MysqlProperties mysqlProperties) {
+    return Jdbi.create(mysqlProperties.getConnectionString(), mysqlProperties.getUser(), mysqlProperties.getPassword());
   }
 
+  @Bean
+  @ConfigurationProperties(prefix = "meshinery.mysql")
+  public MysqlProperties mysqlProperties() {
+    return new MysqlProperties();
+  }
 }
--- modules/mysql-connector/src/main/java/ask/me/again/meshinery/connectors/mysql/MysqlProperties.java
+++ modules/mysql-connector/src/main/java/ask/me/again/meshinery/connectors/mysql/MysqlProperties.java
@@ -4,6 +4,8 @@ import lombok.Data;
 
 @Data
 public class MysqlProperties {
-
   int limit;
+  String user;
+  String password;
+  String connectionString;
 }
