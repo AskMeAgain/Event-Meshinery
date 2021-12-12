29eb81a66f606276a5d789895b6b2d242bb30f77
AskMeAgain <20632124+AskMeAgain@users.noreply.github.com>
2021-12-12 22:16:22 +0100

    0.1.1: Trying to fix the test v3

--- modules/connectors/kafka/meshinery-kafka-connector-spring/src/test/java/io/github/askmeagain/meshinery/connectors/kafka/MeshineryKafkaConfigurationTest.java
+++ modules/connectors/kafka/meshinery-kafka-connector-spring/src/test/java/io/github/askmeagain/meshinery/connectors/kafka/MeshineryKafkaConfigurationTest.java
@@ -1,12 +1,15 @@
 package io.github.askmeagain.meshinery.connectors.kafka;
 
+import io.github.askmeagain.meshinery.connectors.kafka.factories.KafkaProducerFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@MockBean(KafkaProducerFactory.class)
 @SpringJUnitConfig(MeshineryKafkaConfiguration.class)
 @TestPropertySource(properties = {
     "meshinery.connectors.kafka.bootstrap-servers=abc",
