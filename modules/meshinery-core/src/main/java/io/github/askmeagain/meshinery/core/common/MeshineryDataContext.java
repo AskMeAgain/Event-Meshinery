5d5ea69470186aef5a971d11b47a2f373f6d7f68
AskMeAgain <20632124+AskMeAgain@users.noreply.github.com>
2024-08-19 22:38:16 +0200

    Small fixes

--- modules/meshinery-core/src/main/java/io/github/askmeagain/meshinery/core/common/MeshineryDataContext.java
+++ modules/meshinery-core/src/main/java/io/github/askmeagain/meshinery/core/common/MeshineryDataContext.java
@@ -12,7 +12,19 @@ public interface MeshineryDataContext {
    */
   String getId();
 
+  /**
+   * Returns the metadata value for a given key
+   *
+   * @param key the key
+   * @return returns the value if found, returns null if it doesnt
+   */
   String getMetadata(String key);
 
+  /**
+   * Add key/value to metadata for each context
+   * @param key
+   * @param value
+   * @return
+   */
   MeshineryDataContext setMetadata(String key, String value);
 }
